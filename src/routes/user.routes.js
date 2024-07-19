bc09564b1026526d1a4d1c01a29a7a64394a92c5
Nikhil <nikhilgudadhe63@gmail.com>
2024-07-19 14:00:44 +0530

    Auth middle ware added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,9 +2,9 @@ import { Router } from "express";
 import { registerUser, loginUser } from "../controllers/user.controller.js"
 const router = Router()
 
-
-
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
+
+//secured routes
 
 export default router
